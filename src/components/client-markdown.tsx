9b7f2b1028f440a732b953d722a33862f2cf07a1
nicologiacomini <nicolo.giacomini@proton.me>
2024-12-05 21:41:25 +0100

    fix rendering list in ios devices

--- src/components/client-markdown.tsx
+++ src/components/client-markdown.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown'
-import remarkGfm from "remark-gfm";
+// import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
-import rehypeSlug from 'rehype-slug';
+// import rehypeSlug from 'rehype-slug';
 import 'katex/dist/katex.min.css';
 import rehypePrism from "rehype-prism-plus";
-// import '../style/code-style.css'
 import '../style/code-style.css'
 
 export default function ClientMarkdown({ content }: { content: string }) {
@@ -15,14 +14,18 @@ export default function ClientMarkdown({ content }: { content: string }) {
         <section>
         <div className='markdown leading-relaxed font-[Onest-Regular] text-justify max-md:mx-4'>
             <ReactMarkdown
+                components={{
+                    ul: ({node, ...props}) => <ul style={{listStyleType: 'none', paddingLeft: 0}} {...props} />,
+                    ol: ({node, ...props}) => <ol style={{listStyleType: 'none', paddingLeft: '10px'}} {...props} />
+                }}
                 remarkPlugins={[
-                    remarkGfm,
+                    // remarkGfm,
                     remarkMath
                 ]}
                 rehypePlugins={[
                     rehypeRaw,
                     rehypeKatex,
-                    rehypeSlug,
+                    // rehypeSlug,
                     rehypePrism
                 ]}
             >   
