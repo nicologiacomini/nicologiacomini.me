b204c9b0e27688e94708381a42f40064044fb019
nicologiacomini <nicolo.giacomini@proton.me>
2024-12-07 11:49:05 +0100

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,10 +20,10 @@ export default function Header() {
                     <img src="/file.svg" alt="profile" width="50" />
                 </div>
             </header> */}
-            <header className="py-8 px-0 m-0 mb-5">
+            <header className="py-5 px-0 m-0 mb-5">
                 <img src="/profile.png" alt="profile" width="150" className="max-w-full rounded-full float-right mb-5 ml-5 w-[150px] max-xsm:w-[80px]"/>
-                <h1 className="font-[Onest-Bold] mb-1 text-4xl">Nicolò Giacomini</h1>
-                <h4 className="text-a-hover-color">Computer Science Engineer</h4>
+                <h1 className="font-[Onest-Bold] mb-1 text-4xl"><img className="logo mb-2" src="favicon.ico" alt="logo" width="30"/>Nicolò Giacomini</h1>
+                <h4 className="text-a-hover-color font-[Onest-Bold]">Computer Science Engineer</h4>
             </header>
         </>
     );
