384f830db1e7e21d5e0cca18e38561c8c493dc41
nicologiacomini <nicolo.giacomini@proton.me>
2025-08-25 20:47:04 +0200

    update job title in header to reflect current position

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header className="py-5 px-0 m-0 mb-5">
             <Image src="/profile.png" alt="profile" width={150} height={150} className="max-w-full rounded-full float-right mb-5 ml-5 w-[150px] max-xsm:w-[80px]"/>
             <h1 className="font-[Onest-Bold] mb-1 text-4xl"><Image className="logo mb-2" src="/logo.png" alt="logo" height={35} width={35}/>Nicolò Giacomini</h1>
-            <h4 className="text-a-hover-color font-[Onest-Bold]">Computer Science Engineer</h4>
+            <h4 className="text-a-hover-color font-[Onest-Bold]">Computer Engineer (Barcelona)</h4>
         </header>
     );
 };
