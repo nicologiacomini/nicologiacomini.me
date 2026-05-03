e6233c59f1f6c1d6c3cf0c2ac736dfb76b59a83c
nicologiacomini <nicolo.giacomini@proton.me>
2026-05-03 18:33:19 +0200

    Refactor footer and header components for improved styling and responsiveness; update navbar to enable smooth scrolling to contact section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
       <div>
         <a className="card flex justify-between items-center max-w-[400px] mx-auto max-sm:m-5 rounded-lg border border-border-card bg-background-button shadow-sm shadow-shadow-card hover:bg-hover-button" href="https://nicologiacomini.me/rss.xml?cache_bypass=1">
         <div className="flex justify-between items-center mx-5 py-3">
-          <div className="group relative h-8 w-8 rounded-lg mr-4">
-            <TiRss className="h-8 w-8 object-cover" />
+          <div className="group relative h-6 w-6 rounded-lg mr-4">
+            <TiRss className="h-6 w-6 object-cover" />
           </div>
-          <div className="font-[Onest-Light]">
-            Don't miss anything, add the RSS feed to your favourite RSS reader!
+          <div className="font-[Onest-Light] text-sm">
+            Don't miss anything, save the RSS feed!
           </div>
         </div>
         </a>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,17 +4,30 @@ import { Home, Contact, BookOpenText } from 'lucide-react'
 import { Button } from './ui/button';
 import { ModeToggle } from './toggle';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function NavBar() {
     const sizeIcon: string = '20'
+    const pathname = usePathname()
+
+    const handleContactClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        if (pathname === '/') {
+            const elem = document.getElementById('contact');
+            if (elem) {
+                e.preventDefault();
+                elem.scrollIntoView({ behavior: 'smooth' });
+                window.history.pushState(null, '', '/#contact');
+            }
+        }
+    };
 
     return (
         <div className='fixed z-50 bottom-0 w-full align-top h-24 text-center bg-gradient-to-t from-white dark:from-border-card'>
-            <div className='navbar font-[Onest-Regular] bg-background-button rounded-lg inline-flex justify-center items-center gap-1 px-4 py-2 shadow-md shadow-shadow-navbar h-16 border-[0.5px] border-border-card'> 
+            <div className='navbar font-[Onest-Regular] bg-background-button rounded-lg inline-flex justify-center items-center gap-1 px-2 py-0 shadow-md shadow-shadow-navbar h-16 border-[0.5px] border-border-card'> 
                 
                 <Link href='/'>
                     <Button className='navbutton text-left' variant="navbar" size="square">
-                        <div className='max-md:hidden mr-2'></div>
+                        <div className='max-md:hidden'></div>
                         <Home size={sizeIcon} style={{ width: sizeIcon+'px', height: sizeIcon+'px' }} />
                         <div className='navlabel max-md:hidden'>Home</div>
                     </Button>
@@ -22,9 +35,19 @@ export default function NavBar() {
                 
                 <div className='text-2xl'>|</div>
                 
+                <Link href='/#contact' onClick={handleContactClick}>
+                    <Button className='navbutton text-left' variant="navbar" size="square">
+                        <div className='max-md:hidden'></div>
+                        <Contact size={sizeIcon} style={{ width: sizeIcon+'px', height: sizeIcon+'px' }} />
+                        <div className='navlabel max-md:hidden'>Contact</div>
+                    </Button>
+                </Link>
+
+                <div className='text-2xl'>|</div>
+                
                 <Link href='/posts'>
                     <Button className='navbutton text-left' variant="navbar" size="square">
-                        <div className='max-md:hidden mr-2'></div>
+                        <div className='max-md:hidden'></div>
                         <BookOpenText size={sizeIcon} style={{ width: sizeIcon+'px', height: sizeIcon+'px' }} />
                         <div className='navlabel max-md:hidden'>Posts</div>
                     </Button>
@@ -32,15 +55,6 @@ export default function NavBar() {
                 
                 <div className='text-2xl'>|</div>
 
-                <Link href='/contact'>
-                    <Button className='navbutton text-left' variant="navbar" size="square">
-                        <div className='max-md:hidden mr-2'></div>
-                        <Contact size={sizeIcon} style={{ width: sizeIcon+'px', height: sizeIcon+'px' }} />
-                        <div className='navlabel max-md:hidden'>Contact</div>
-                    </Button>
-                </Link>
-
-                <div className='text-2xl'>|</div>
                 <ModeToggle sizeIcon={sizeIcon}/>
             </div>
         </div>
